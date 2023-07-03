9efd53f1d6b7263eac6926dd687d4c34359e35b9
Matthew Summers <imadethisforgithub@gmail.com>
2023-07-03 14:23:44 -0500

    added is_anagram

---
+++ string_fun.py
@@ -0,0 +1,34 @@
+def is_palindrome(input_string):
+    # return input_string == input_string[::-1]  ## simple way to do it
+
+    # Algorithmic way
+    start = 0
+    end = len(input_string)-1
+
+    while input_string[start] == input_string[end]:
+        if start > end//2:
+            return True
+        start +=1
+        end -= 1
+    return False
+print(is_palindrome("saippuakivikauppias")) # True
+print(is_palindrome("apples")) # False
+
+def is_anagram(str1, str2):
+    dict1 = {}
+    dict2 = {}
+
+    for char in str1:
+        char = char.lower()
+        dict1[char] = dict1.get(char, 0) + 1
+        
+    for char in str2:
+        char = char.lower()
+        dict2[char] = dict2.get(char, 0) + 1
+
+    return dict1 == dict2
+
+print(is_anagram("angel", "glean")) # True
+print(is_anagram("angel", "GLEAN")) # True
+print(is_anagram("angel", "gleann")) # False
+print(is_anagram("Pickles", "Eggplant")) # False
